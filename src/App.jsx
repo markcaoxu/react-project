4305c3e738c0898b8eb340df7e38b762d02f26f9
sword <markcaoxu@163.com>
2019-09-17 19:12:03 +0800

    Router路由

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React,{ Component } from 'react';
+// 引入路由，BrowserRouter as Router，使用as进行重命名
+import { BrowserRouter as Router } from 'react-router-dom';
+
+class App extends Component {
+  render() {
+    return <Router>
+      App
+    </Router>
+  }
+}
+
+export default App;
